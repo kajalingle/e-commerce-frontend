900112132ad028a2e4a2f436c5673a42f86f65c7
Kajal Ingle <kajalingle627@gmail.com>
2026-09-17 23:43:13 +0530

    no message

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import Navigation from "./customer/component/Navigation";
+import HomePage from "./customer/Pages/HomePage/HomePage";
 
 function App() {
   return (
     <div className="App">
            <Navigation />
+    <div>
+      <HomePage/>
     </div>
+    </div>
+
   );
 }
 
