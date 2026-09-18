3b0312628f75123d053a4d479a45183ee79d3a14
Kajal Ingle <kajalingle627@gmail.com>
2026-09-18 22:58:28 +0530

    no message

--- src/customer/component/HomeCarosel/MainCarosel.jsx
+++ src/customer/component/HomeCarosel/MainCarosel.jsx
@@ -1,20 +1,23 @@
-import React from 'react';
-import AliceCarousel from 'react-alice-carousel';
-import 'react-alice-carousel/lib/alice-carousel.css';
-import { mainCarouselData } from './MainCarosalData';
+import React from "react";
+import { mainCarouselData } from "./MainCarosalData";
+import AliceCarousel from "react-alice-carousel";
 
+const MainCarosel = () => {
 
-const Maincarosel = () => (
-
-    // const navigate=use
-
-    const items = mainCarouselData.map((item)=> <img className='cursor-pointer' onClick ={()=} src={item.image} alt=""/>)
-
-    <AliceCarousel
-        mouseTracking
-        items={items}
-        controlsStrategy="alternate"
+  const items = mainCarosalData.map((item) => (
+    <img
+      className="cursor-pointer"
+      role="presentation"
+      src={item.image}
+      alt=""
     />
-);
+  ));
 
-export default Maincarosel;
+  return (
+    <AliceCarousel
+      items={items}
+    />
+  );
+};
+
+export default MainCarosel;
