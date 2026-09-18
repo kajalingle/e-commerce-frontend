0031570ee40aace0e423abac44d10e7491fe2f86
Kajal Ingle <kajalingle627@gmail.com>
2026-09-18 22:24:56 +0530

    no message

--- src/customer/component/HomeCarosel/MainCarosel.jsx
+++ src/customer/component/HomeCarosel/MainCarosel.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import AliceCarousel from 'react-alice-carousel';
 import 'react-alice-carousel/lib/alice-carousel.css';
+import { mainCarouselData } from './MainCarosalData';
 
-
-const items = [
-    <div className="item" data-value="1">1</div>,
-    <div className="item" data-value="2">2</div>,
-    <div className="item" data-value="3">3</div>,
-    <div className="item" data-value="4">4</div>,
-    <div className="item" data-value="5">5</div>,
-];
 
 const Maincarosel = () => (
+
+    // const navigate=use
+
+    const items = mainCarouselData.map((item)=> <img className='cursor-pointer' onClick ={()=} src={item.image} alt=""/>)
+
     <AliceCarousel
         mouseTracking
         items={items}
